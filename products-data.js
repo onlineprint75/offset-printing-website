24f45dab812109eb08223b2d07311f9fdb0cc1b9
onlineprint75 <81549703+onlineprint75@users.noreply.github.com>
2026-01-07 13:29:27 +0800

    Hide Roll Length field for Sticker product via formConfig

--- products-data.js
+++ products-data.js
@@ -26,6 +26,7 @@ const PRODUCTS_DATA = [
     isLamination: true,
     // Custom form config for Sticker
     formConfig: {
+      showRollLength: false,
       quantities: [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 6000, 7000, 8000, 9000, 10000],
       finishOptions: [
         { value: "glossy", i18nKey: "form-sticker-glossy", enabled: true },
